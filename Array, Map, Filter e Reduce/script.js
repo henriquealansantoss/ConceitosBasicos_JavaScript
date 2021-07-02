aecfa51dc352ee9b43d45d065f16e4390d53a5b4
henriquealansantoss <henriquealansantoss@gmail.com>
2021-07-01 22:18:48 -0300

    Revisão de conceitos - Origamid

--- Array, Map, Filter e Reduce/script.js
+++ Array, Map, Filter e Reduce/script.js
@@ -10,13 +10,12 @@ const precos = [
 
 // filter - filtra os valores de um array conforme o que for solicitado na arrow function
 const precoFilter = precos.filter(item => item.includes("R$"))
-
+console.log(precoFilter);
 // map - retorna todos os itens de um array = neste caso neste caso convertendo as strings para numeros
 const precoMap = precoFilter.map(item => Number(item.replace("R$", "")))
-
+console.log(precoMap);
 // reduce - pega o item anterior no primeiro parametro = neste caso está sendo feito uma soma sempre
 // item anterior com o item atual no array (pode ter valor indicado para inicio 
 // ex: precoMap.reduce((acc, item) => { return acc + item}, 0))
 const precoReduce = precoMap.reduce((acc, item) => acc + item)
-
 console.log(precoReduce);
---
+++ Expressões/script.js
@@ -0,0 +1,20 @@
+const grupoA = 100;
+const grupoB = 300;
+
+// Exemplo de expressoes
+
+const vencedor = grupoA > grupoB ? "Grupo A Venceu" : "Grupo B Venceu" // expressão
+console.log(vencedor);
+
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter(numero => numero > 4); // expressão
+console.log(total);
+
+
+const active = true;
+const button = active && "Botão está ativo"; // expressão
+console.log(button);
+
+
+
